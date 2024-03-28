371128ea5b7d4c9a25a73b4dd13dae26ca1fdcb6
Aleksandr Vakulenko <sasha705@mail.ru>
2024-03-28 11:39:42 +0300

    update lab5

--- Lab_5/1_new_dispose.pas
+++ Lab_5/1_new_dispose.pas
@@ -1,10 +1,28 @@
-program new_try;
+program prt_demo;
 {$APPTYPE CONSOLE}
+
+uses
+    SysUtils;
 
 var
 	i_ptr : ^integer;
 begin
+    writeln(format('Address: %p Value: %u', [i_ptr, i_ptr^]));
+    
+    i_ptr^ := 87;
+    writeln(format('Address: %p Value: %u', [i_ptr, i_ptr^]));
+
 	New(i_ptr);
-	writeln(format('Address: %p', [i_ptr]));
-	Dispose(i_ptr[i]);
+    writeln(format('Address: %p Value: %u', [i_ptr, i_ptr^]));
+
+	Dispose(i_ptr);
+    writeln(format('Address: %p Value: %u', [i_ptr, i_ptr^]));
+
+    i_ptr^ := 88;
+    writeln(format('Address: %p Value: %u', [i_ptr, i_ptr^]));
+
+    i_ptr := nil;
+    writeln(format('Address: %p Value: ---', [i_ptr]));
+
+    readln;
 end.
